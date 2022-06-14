55478466cf0f3540ff7ffae4ef2e6816133cea00
Joy Evans <joyevans@me.com>
2022-06-14 10:23:07 -0500

    created prescription model

---
+++ HealthSquad/Models/Prescription.cs
@@ -0,0 +1,14 @@
+namespace Models;
+
+public class Prescription {
+    public int Id { get; set;}
+    private User? _user;
+    public User? User
+    {
+        get => _user;
+        set
+        {
+            
+        }
+    }
+}
