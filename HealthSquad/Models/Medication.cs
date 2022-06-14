812d4864b736edee6d88dc7c562b2709760b9dcb
Joy Evans <joyevans@me.com>
2022-06-14 10:12:16 -0500

    created medication model

---
+++ HealthSquad/Models/Medication.cs
@@ -0,0 +1,50 @@
+﻿namespace Models;
+public class Medication
+{
+    public int Id { get; set; }
+    private string? _name;
+    public string? Name
+    {
+        get => _name;
+        set
+        {
+
+        }
+    }
+    private string? _description;
+    public string? Description
+    {
+        get => _description;
+        set
+        {
+
+        }
+    }
+    private string? _dosage;
+    public string? Dosage
+    {
+        get => _dosage;
+        set
+        {
+
+        }
+    }
+    private string? _directions;
+    public string? Directions
+    {
+        get => _directions;
+        set
+        {
+
+        }
+    }
+    private int _count;
+    public int Count
+    {
+        get => _count;
+        set
+        {
+
+        }
+    }
+}
