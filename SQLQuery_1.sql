84e9b0f1e4cf299412c3e59827ae79f9e4115f37
Joy Evans <joyevans@me.com>
2022-06-16 17:01:06 -0500

    created db

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -7,3 +7,32 @@ SET AUTO_CLOSE OFF;
 
 -- changing database to use HSDB as default instead of master
 USE HSDB;
+
+CREATE TABLE [User](
+    ID INT PRIMARY KEY,
+    Username NVARCHAR(MAX),
+    Password NVARCHAR(60)
+    -- https://docs.microsoft.com/en-us/sql/relational-databases/security/strong-passwords?view=sql-server-ver16
+);
+
+CREATE TABLE Medication(
+    ID INT PRIMARY KEY,
+    Name NVARCHAR(MAX),
+    Description NVARCHAR(MAX),
+    Dosage NVARCHAR(MAX),
+    Directions NVARCHAR(MAX),
+    Count INT
+);
+
+CREATE TABLE Prescription(
+    ID INT PRIMARY KEY
+    UserID INT FOREIGN KEY REFERENCES [User](ID) NOT NULL,
+    MedicationID INT FOREIGN KEY REFERENCES Medication(ID) NOT NULL,
+);
+
+CREATE TABLE Reminder(
+    ID INT PRIMARY KEY,
+    UserID INT FOREIGN KEY REFERENCES [User](ID) NOT NULL,
+    PrescriptionID INT FOREIGN KEY REFERENCES Prescription(ID) NOT NULL,
+    Alert TIME
+);
