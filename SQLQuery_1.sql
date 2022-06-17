fb3dd962f3bdbdc7d2123fc1fe027f5eee52d399
Joy Evans <joyevans@me.com>
2022-06-17 15:11:59 -0500

    created tables in db

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -11,28 +11,27 @@ USE HSDB;
 CREATE TABLE [User](
     ID INT PRIMARY KEY,
     Username NVARCHAR(MAX),
-    Password NVARCHAR(60)
-    -- https://docs.microsoft.com/en-us/sql/relational-databases/security/strong-passwords?view=sql-server-ver16
+    Password NVARCHAR(60) NOT NULL
 );
 
 CREATE TABLE Medication(
     ID INT PRIMARY KEY,
-    Name NVARCHAR(MAX),
+    Name NVARCHAR(MAX) NOT NULL,
     Description NVARCHAR(MAX),
     Dosage NVARCHAR(MAX),
     Directions NVARCHAR(MAX),
-    Count INT
+    Count INT NOT NULL CHECK (Count>=1) DEFAULT 1
 );
 
 CREATE TABLE Prescription(
-    ID INT PRIMARY KEY
+    ID INT PRIMARY KEY,
     UserID INT FOREIGN KEY REFERENCES [User](ID) NOT NULL,
-    MedicationID INT FOREIGN KEY REFERENCES Medication(ID) NOT NULL,
+    MedicationID INT FOREIGN KEY REFERENCES Medication(ID) NOT NULL
 );
 
 CREATE TABLE Reminder(
     ID INT PRIMARY KEY,
     UserID INT FOREIGN KEY REFERENCES [User](ID) NOT NULL,
     PrescriptionID INT FOREIGN KEY REFERENCES Prescription(ID) NOT NULL,
-    Alert TIME
+    Alert TIME NOT NULL
 );
