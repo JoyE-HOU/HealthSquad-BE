d9202bdd6f3326ab058b648af592dfe3ad68f6ff
Joy Evans <joyevans@me.com>
2022-06-27 15:32:31 -0500

    created more tests

--- HealthSquad/Tests/UnitTest1.cs
+++ HealthSquad/Tests/UnitTest1.cs
@@ -12,4 +12,25 @@ public class UnitTest1
         
         Assert.NotNull(testUser);
     }
+    [Fact]
+    public void MedicationShouldCreate()
+    {
+        Medication testMedication = new Medication();
+
+        Assert.NotNull(testMedication);
+    }
+    [Fact]
+    public void PrescriptionShouldCreate()
+    {
+        Prescription testPrescription = new Prescription();
+
+        Assert.NotNull(testPrescription);
+    }
+    [Fact]
+    public void ReminderShouldCreate()
+    {
+        Reminder testReminder = new Reminder();
+
+        Assert.NotNull(testReminder);
+    }
 }
